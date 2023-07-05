b0e6c57e22b66badbd56e9d0bcf569c5cc1d1d38
wangxu <hygge_wx@163.com>
2023-07-05 15:15:08 +0800

    first

--- src/main/java/com/example/autotest/util/MethodUtil.java
+++ src/main/java/com/example/autotest/util/MethodUtil.java
@@ -6,25 +6,4 @@ public class MethodUtil {
         StringBuilder sb = new StringBuilder(str);
         return sb.reverse().toString();
     }
-
-    public static void demo2() {
-
-    }
-
-    public static void demo3() {
-
-    }
-
-    public static void demo4() {
-
-    }
-
-    public static void demo5() {
-
-    }
-
-    public static void demo6() {
-
-    }
-
 }
